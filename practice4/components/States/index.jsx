17a4a2d096347cf11a75a1b691e31e5015c841cf
daniel1876-zhuo <danielzhuo200793@163.com>
2026-04-16 01:26:07 +0800

    problem 2 finish

--- practice4/components/States/index.jsx
+++ practice4/components/States/index.jsx
@@ -13,10 +13,44 @@ class States extends React.Component {
       "window.cs142models.statesModel()",
       window.cs142models.statesModel()
     );
+    const modelData = window.cs142models;
+    this.handleStatesInputChange = this.handleStatesInputChange.bind(this);
+    this.state = {
+      statesModel:modelData.statesModel(),
+      statesModelMatched:[],
+      isAnyStateMatched:false,
+    };
   }
-
+  handleStatesInputChange(event){
+    const text = event.target.value;
+    let statesModelMatched = [];
+    this.setState({isAnyStateMatched:false});
+    for(let i = 0;i < this.state.statesModel.length;i++){
+      const stateModel = this.state.statesModel[i];
+      if(stateModel.includes(text) === true){
+        statesModelMatched.push(stateModel);
+        this.setState({isAnyStateMatched:true});
+      }
+    }
+    this.setState({statesModelMatched:statesModelMatched});
+    console.log(`statesModelMatched:${statesModelMatched}`);
+  }
   render() {
-    return <div>Replace this with the code for CS142 Project 4, Problem 2</div>;
+    let matchedStateItems = [];
+    for(let i = 0;i < this.state.statesModelMatched.length;i++){
+      matchedStateItems.push(<li key={this.state.statesModelMatched[i]}>{this.state.statesModelMatched[i]}</li>);
+    }
+    return <div className="statesFinder">
+      {/* Replace this with the code for CS142 Project 4, Problem 2 */}
+      <br></br>
+      Type the states you want to find:
+      <input className="statesInput" type="text" 
+       onChange={this.handleStatesInputChange}></input>
+       <div className="statesList">
+        {this.state.isAnyStateMatched?<ul>{matchedStateItems}</ul>:"No state is matched."}
+       </div>
+      </div>;
+
   }
 }
 
